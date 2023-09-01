3cca17d60435b75d40769bd224eb0e8b4fc071eb
SimonCoteWeb <simoncote.web@gmail.com>
2023-09-01 10:46:22 -0400

    Fix front end bug on prod build.

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-
-import Banner from "./banner/banner";
-import Url from "./url/url";
 import NavBar from "./navBar/navBar";
 
 import background from "../../global/assets/images/2560x5120_leeroyer_HOME.jpg";
@@ -9,8 +6,17 @@ import background from "../../global/assets/images/2560x5120_leeroyer_HOME.jpg";
 function Home() {
   return (
     <div className="">
-      <Banner />
-      <Url />
+      <div
+        className="h-28 text-7xl text top-1/2 w-full bg-white fixed flex justify-center items-center -translate-y-1/2 font-conduitExtralight
+    lg:h-36 lg:text-8xl"
+      >
+        LEE ROYER
+      </div>
+      <div className=" pl-4 h-12 bg-transparent fixed flex w-full items-center">
+        <div className=" text-2xl lg:text-4xl font-conduitLight">
+          leeroyer.com
+        </div>
+      </div>
       <NavBar />
       <img
         className="min-h-screen overflow-x-hidden"
@@ -22,4 +28,3 @@ function Home() {
 }
 
 export default Home;
-// sm:max-h-none:block:mx-auto:w-full
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { AuthProvider } from "./context/AuthProvider";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 if (process.env.NODE_ENV === "production") {
@@ -11,12 +11,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
     </AuthProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
